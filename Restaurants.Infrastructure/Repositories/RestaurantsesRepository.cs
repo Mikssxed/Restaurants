bc97d1e10bd8525d47158557a2b4954dd70f7f1e
Mikssxed <eksilonxd@gmail.com>
2025-11-18 18:33:44 +0100

    Implement restaurants repository and service with controller for restaurant management

---
+++ Restaurants.Infrastructure/Repositories/RestaurantsesRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Repositories;
+
+public class RestaurantsesRepository(RestaurantDbContext dbContext) : IRestaurantsRepository
+{
+    public async Task<IEnumerable<Restaurant>> GetAllAsync()
+    {
+        var restaurants = await dbContext.Restaurants.ToListAsync();
+        return restaurants;
+    }
+
+    public Task<Restaurant?> GetByIdAsync(int id)
+    {
+        var restaurant = dbContext.Restaurants.FirstOrDefaultAsync(r => r.Id == id);
+        return restaurant;
+    }
+}
