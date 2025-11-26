a01c1eabf385c1b92e1b523a2a7818e1e24a934c
Mikssxed <eksilonxd@gmail.com>
2025-11-26 21:00:39 +0100

    Implement pagination and sorting for restaurant retrieval, add query validator and update repository methods

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Restaurants.Application.Restaurants.Dtos;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private readonly int[] allowedPageSizes = [5, 10, 20, 50];
+    private readonly string[] allowedSortByColumns = [nameof(RestaurantDto.Name), nameof(RestaurantDto.Description), nameof(RestaurantDto.Category)];
+
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
+        RuleFor(r => r.PageSize).Must(value => allowedPageSizes.Contains(value)).WithMessage($"Page size must be in [{string.Join(",", allowedPageSizes)}");
+
+        RuleFor(r => r.SortBy).Must(value => allowedSortByColumns.Contains(value)).When(q => q.SortBy != null)
+            .WithMessage($"Sort by is optional or must be in [{string.Join(",", allowedSortByColumns)}");
+    }
+}
--- Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
+++ Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Constants;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Persistence;
@@ -18,7 +20,7 @@ public class RestaurantsRepository(RestaurantDbContext dbContext) : IRestaurants
         var restaurant = dbContext.Restaurants.Include(r => r.Dishes).FirstOrDefaultAsync(r => r.Id == id);
         return restaurant;
     }
-    
+
     public async Task<int> Create(Restaurant restaurant)
     {
         dbContext.Restaurants.Add(restaurant);
@@ -31,10 +33,44 @@ public class RestaurantsRepository(RestaurantDbContext dbContext) : IRestaurants
         dbContext.Restaurants.Remove(restaurant);
         await dbContext.SaveChangesAsync();
     }
-    
+
     public async Task Update(Restaurant restaurant)
     {
         dbContext.Restaurants.Update(restaurant);
         await dbContext.SaveChangesAsync();
+    }
+
+    public async Task<(IEnumerable<Restaurant>, int)> GetAllMatchingAsync(string? searchPhrase, int pageNumber, int pageSize, string? sortBy,
+        SortDirection sortDirection)
+    {
+        var searchPhraseLower = searchPhrase?.ToLower();
+
+        var baseQuery = dbContext.Restaurants
+            .Where(r => (searchPhraseLower != null && r.Name.ToLower().Contains(searchPhraseLower)) || r.Description.ToLower().Contains(searchPhraseLower));
+
+        var totalCount = await baseQuery.CountAsync();
+
+        if (sortBy != null)
+        {
+            var columnsSelector = new Dictionary<string, Expression<Func<Restaurant, object>>>
+            {
+                { nameof(Restaurant.Name), r => r.Name },
+                { nameof(Restaurant.Description), r => r.Description },
+                { nameof(Restaurant.Category), r => r.Category }
+            };
+
+            var selectedColumn = columnsSelector[sortBy];
+
+            baseQuery = sortDirection == SortDirection.Ascending
+                ? baseQuery.OrderBy(selectedColumn)
+                : baseQuery.OrderByDescending(selectedColumn);
+        }
+
+        var restaurants = await baseQuery
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return (restaurants, totalCount);
     }
 }
