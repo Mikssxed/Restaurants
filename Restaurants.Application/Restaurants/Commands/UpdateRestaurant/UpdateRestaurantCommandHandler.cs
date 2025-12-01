d6d1aaa218de9697872428a7d766959a61f06d0a
Mikssxed <eksilonxd@gmail.com>
2025-12-01 21:23:35 +0100

    Add a blank line for improved readability in UpdateRestaurantCommandHandler

--- Restaurants.Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandler.cs
@@ -21,6 +21,7 @@ public class UpdateRestaurantCommandHandler(
         var restaurant = await restaurantsRepository.GetByIdAsync(request.Id);
         if (restaurant is null) throw new NotFoundException(nameof(Restaurant), request.Id.ToString());
 
+
         if (!restaurantAuthorizationService.Authorize(restaurant, ResourceOperation.Update)) throw new ForbidException();
 
         mapper.Map(request, restaurant);
